f33ec5f6451ea9577f0b031f3d5e642741d34a9e
gabrielVais <gabriel_lol2017@hotmail.com>
2019-09-17 01:00:11 -0300

    começando a aprender lua exibindo dados com print e variaveis tabelas e funcoes recursivas

---
+++ introducao/valores-variaveis-typos.lua
@@ -0,0 +1,49 @@
+a = 3 --number
+
+b = "agua" -- string
+
+-- nill Not in list = valor inexistente
+
+--number: numeros em lua sao float por default
+
+x = 1
+
+y = 2.5
+
+address = 0x0000aaf8
+
+print(x + y)
+
+-- string : textos
+
+endereco = "Av Maracana, 255"
+
+telefone = "(14)3732-9384"
+
+print("Meu telefone eh",telefone)
+print("Meu endereco eh", endereco)
+
+-- table: Tabelas -> tipo de dados parecido com objeto
+
+pessoa = {
+
+	nome = "Eric",
+	idade = "26"
+
+}
+
+print(pessoa.nome)
+
+-- array por tabela
+pares = {0, 2, 4, 6, 8, 10}
+
+
+-- funçoes recursiva em lua
+function soma(a, b)
+
+	return a + b
+
+end
+
+
+print(soma(20, 2))
